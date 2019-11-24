a468a00b31d407497b9746b29071bb898b6d98e3
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-11-25 04:25:13 +0900

    Create searchBroadcaster resolver

---
+++ src/api/Broadcaster/searchBroadcaster/searchBroadcaster.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../generated/prisma";
+
+/**
+ * ================================================================
+ * 브로드캐스터 검색
+ * ================================================================
+ * 수정예정
+ * ================================================================
+ */
+
+export default {
+  Query: {
+    searchBroadcaster: async (_, args) => {
+      const { bName } = args;
+      return await prisma.broadcasters({ where: { bName } });
+    }
+  }
+};
