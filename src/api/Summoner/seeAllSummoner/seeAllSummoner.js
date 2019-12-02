ff64572cfb126fa701586751cb1969ba6d321e78
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-12-02 12:37:59 +0900

    Refactor seeAllSummoner resolver

--- src/api/Summoner/seeAllSummoner/seeAllSummoner.js
+++ src/api/Summoner/seeAllSummoner/seeAllSummoner.js
@@ -11,139 +11,40 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     seeAllSummoner: async (_, __) => {
-      const unrankedTier = await prisma.summoners({
-        where: { sTier: "UNRANKED" }
+      const unsortedSummoners = await prisma.summoners();
+
+      let sortBy = [
+        {
+          prop: "sTierNum",
+          direction: 1
+        },
+        {
+          prop: "sRank",
+          direction: 1
+        },
+        {
+          prop: "sPoints",
+          direction: -1
+        }
+      ];
+
+      const sortedSummoners = unsortedSummoners.sort(function(a, b) {
+        let i = 0,
+          result = 0;
+        while (i < sortBy.length && result === 0) {
+          result =
+            sortBy[i].direction *
+            (a[sortBy[i].prop].toString() < b[sortBy[i].prop].toString()
+              ? -1
+              : a[sortBy[i].prop].toString() > b[sortBy[i].prop].toString()
+              ? 1
+              : 0);
+          i++;
+        }
+        return result;
       });
-      const ironTier = await prisma.summoners({ where: { sTier: "IRON" } });
-      const bronzeTier = await prisma.summoners({ where: { sTier: "BRONZE" } });
-      const silverTier = await prisma.summoners({ where: { sTier: "SILVER" } });
-      const goldTier = await prisma.summoners({ where: { sTier: "GOLD" } });
-      const platinumTier = await prisma.summoners({
-        where: { sTier: "PLATINUM" }
-      });
-      const diamondTier = await prisma.summoners({
-        where: { sTier: "DIAMOND" }
-      });
-      const masterTier = await prisma.summoners({
-        where: { sTier: "MASTER" }
-      });
-      const grandmasterTier = await prisma.summoners({
-        where: { sTier: "GRANDMASTER" }
-      });
-      const challengerTier = await prisma.summoners({
-        where: { sTier: "CHALLENGER" }
-      });
-      const ironSort = ironTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const bronzeSort = bronzeTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const silverSort = silverTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const goldSort = goldTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const platinumSort = platinumTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const diamondSort = diamondTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const masterSort = masterTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const grandmasterSort = grandmasterTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const challengerSort = challengerTier.sort((a, b) => {
-        let o1 = b["sRank"];
-        let o2 = a["sRank"];
-        let p1 = a["sPoints"];
-        let p2 = b["sPoints"];
-        if (o1 > o2) return -1;
-        if (o1 < o2) return 1;
-        if (p1 > p2) return -1;
-        if (p1 < p2) return 1;
-        return 0;
-      });
-      const summoners = challengerSort.concat(
-        grandmasterSort,
-        masterSort,
-        diamondSort,
-        platinumSort,
-        goldSort,
-        silverSort,
-        bronzeSort,
-        ironSort,
-        unrankedTier
-      );
-      return summoners;
+
+      return sortedSummoners;
     }
   }
 };
