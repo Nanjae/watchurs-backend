f15b32e2a1ba3b8dcfe5cb7b7e324391cdd53ee6
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-12-10 22:59:57 +0900

    Update createSummoner resolver

--- src/api/Summoner/createSummoner/createSummoner.js
+++ src/api/Summoner/createSummoner/createSummoner.js
@@ -87,74 +87,23 @@ let sTierNum = 99;
 
 export default {
   Mutation: {
-    setSummoner: async (_, args) => {
+    createSummoner: async (_, args) => {
       const { sNameS, bId } = args;
       const RIOT_API = process.env.RIOT_API;
       const bInfo = await prisma.broadcaster({ bId });
       const encodedSNameS = encodeURIComponent(sNameS);
       const {
-        data: { id: sId, profileIconId: sAvatar }
+        data: { id: sId, profileIconId: sAvatar, accountId: sAccountId }
       } = await axios.get(
         `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedSNameS}?api_key=${RIOT_API}`
       );
       const sAvatarUrl = `https://ddragon.leagueoflegends.com/cdn/9.23.1/img/profileicon/${sAvatar}.png`;
       const existSummoner = await prisma.$exists.summoner({ sId });
-      // 조건 1 : 등록된 소환사 ID
-      if (existSummoner) {
-        const [{ id }] = await prisma.summoners({ where: { sId } });
-        try {
-          // 조건 1-1 : 랭크 정보가 있는 소환사
-          try {
-            const {
-              summonerName: sName,
-              tier: sTier,
-              rank: sRank,
-              leaguePoints: sPoints,
-              wins: sWins,
-              losses: sLosses
-            } = await getRankedData(sId, RIOT_API);
-            setSTierNum(sTier);
-            await prisma.updateSummoner({
-              where: { id },
-              data: {
-                sName,
-                sAvatar: sAvatarUrl,
-                sTier,
-                sTierNum,
-                sRank,
-                sPoints,
-                sWins,
-                sLosses,
-                sBroadcaster: { connect: { id: bInfo.id } }
-              }
-            });
-            return true;
-          } catch (e) {
-            // 조건 1-2 : 랭크 정보가 없는 소환사
-            try {
-              const { name: sName } = await getUnrankedData(sId, RIOT_API);
-              await prisma.updateSummoner({
-                where: { id },
-                data: {
-                  sName,
-                  sAvatar: sAvatarUrl,
-                  sBroadcaster: { connect: { id: bInfo.id } }
-                }
-              });
-              return true;
-            } catch (e) {
-              console.log(e);
-              return false;
-            }
-          }
-        } catch (e) {
-          console.log(e);
-          return false;
-        }
-        // 조건 2 : 등록되지 않은 소환사 ID
+
+      if (!existSummoner) {
+        throw error("이미 등록된 소환사입니다.");
       } else {
         try {
-          // 조건 2-1 : 랭크 정보가 있는 소환사
           try {
             const {
               summonerName: sName,
@@ -165,29 +114,60 @@ export default {
               losses: sLosses
             } = await getRankedData(sId, RIOT_API);
             setSTierNum(sTier);
-            await prisma.createSummoner({
-              sId,
-              sName,
-              sAvatar: sAvatarUrl,
-              sTier,
-              sTierNum,
-              sRank,
-              sPoints,
-              sWins,
-              sLosses,
-              sBroadcaster: { connect: { id: bInfo.id } }
+            const {
+              data: { matches }
+            } = await axios.get(
+              `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${sAccountId}?endIndex=1&api_key=${RIOT_API}`
+            );
+
+            matches.map(async match => {
+              const {
+                data: {
+                  participantIdentities,
+                  gameMode,
+                  gameType,
+                  participants
+                }
+              } = await axios.get(
+                `https://kr.api.riotgames.com/lol/match/v4/matches/${match.gameId}?api_key=${RIOT_API}`
+              );
+              const arrayIndex = participantIdentities.findIndex(
+                x => x.player.accountId === sAccountId
+              );
+              console.log("모드 : " + gameMode);
+              console.log("타입 : " + gameType);
+              console.log("라인 : " + participants[arrayIndex].timeline.lane);
+              console.log("역할 : " + participants[arrayIndex].timeline.role);
+              console.log("킬 : " + participants[arrayIndex].stats.kills);
+              console.log("데스 : " + participants[arrayIndex].stats.deaths);
+              console.log("어시 : " + participants[arrayIndex].stats.assists);
+              console.log("승패 : " + participants[arrayIndex].stats.win);
+              console.log("챔피언 : " + participants[arrayIndex].championId);
             });
+
+            // await prisma.createSummoner({
+            //   sId,
+            //   sName,
+            //   sAvatar: sAvatarUrl,
+            //   sTier,
+            //   sTierNum,
+            //   sRank,
+            //   sPoints,
+            //   sWins,
+            //   sLosses,
+            //   sBroadcaster: { connect: { id: bInfo.id } }
+            // });
             return true;
           } catch (e) {
             // 조건 2-2 : 랭크 정보가 없는 소환사
             try {
               const { name: sName } = await getUnrankedData(sId, RIOT_API);
-              await prisma.createSummoner({
-                sId,
-                sName,
-                sAvatar: sAvatarUrl,
-                sBroadcaster: { connect: { id: bInfo.id } }
-              });
+              // await prisma.createSummoner({
+              //   sId,
+              //   sName,
+              //   sAvatar: sAvatarUrl,
+              //   sBroadcaster: { connect: { id: bInfo.id } }
+              // });
               return true;
             } catch (e) {
               console.log(e);
