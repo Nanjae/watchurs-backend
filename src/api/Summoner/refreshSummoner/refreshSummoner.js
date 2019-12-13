5651435ad3e8f60ae1fa2ad6fe6d02f474456868
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-12-13 17:42:56 +0900

    Update refreshSummoner resolver

--- src/api/Summoner/refreshSummoner/refreshSummoner.js
+++ src/api/Summoner/refreshSummoner/refreshSummoner.js
@@ -59,16 +59,28 @@ const setSTierNum = async sTier => {
   }
 };
 
+const delayAPI = item => {
+  return new Promise(resolve =>
+    setTimeout(() => {
+      console.log(new Date());
+      console.log(item);
+      resolve();
+    }, 3000)
+  );
+};
+
 export default {
   Mutation: {
     refreshSummoner: async (_, args) => {
-      const { id, sId } = args;
+      // 소환사정보 업데이트
+      const { sId } = args;
       const RIOT_API = process.env.RIOT_API;
       const {
-        data: { name: sName, profileIconId: sAvatar }
+        data: { name: sName, profileIconId: sAvatar, accountId: sAccountId }
       } = await axios.get(
         `https://kr.api.riotgames.com/lol/summoner/v4/summoners/${sId}?api_key=${RIOT_API}`
       );
+      await delayAPI("소환사 기본정보 호출 완료");
       const sAvatarUrl = `https://ddragon.leagueoflegends.com/cdn/9.23.1/img/profileicon/${sAvatar}.png`;
       try {
         const {
@@ -78,10 +90,12 @@ export default {
           wins: sWins,
           losses: sLosses
         } = await getRankedData(sId, RIOT_API);
+        await delayAPI("소환사 랭크정보 호출 완료");
         setSTierNum(sTier);
         await prisma.updateSummoner({
-          where: { id },
+          where: { sId },
           data: {
+            sAccountId,
             sName,
             sAvatar: sAvatarUrl,
             sTier,
@@ -92,13 +106,12 @@ export default {
             sLosses
           }
         });
-        return true;
       } catch (e) {
-        // 조건 1-2 : 랭크 정보가 없는 소환사
         try {
           await prisma.updateSummoner({
-            where: { id },
+            where: { sId },
             data: {
+              sAccountId,
               sName,
               sAvatar: sAvatarUrl,
               sTier: "UNRANKED",
@@ -109,12 +122,114 @@ export default {
               sLosses: 0
             }
           });
-          return true;
         } catch (e) {
           console.log(e);
           return false;
         }
       }
+
+      // 게임ID 업데이트
+      let preGameId = new Array(20);
+      let newGameId = new Array(20);
+      let addGameId = new Array();
+      let delGameId = new Array();
+
+      const existSDetail = await prisma.$exists.detail({ dSummoner: { sId } });
+      if (existSDetail) {
+        const preDetail = await prisma.summoner({ sId }).sDetail();
+        preDetail.map((detail, index) => {
+          preGameId[index] = detail.dGameId;
+        });
+      }
+
+      const {
+        data: { matches }
+      } = await axios.get(
+        `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${sAccountId}?queue=0&queue=400&queue=420&queue=430&queue=440&queue=700&endIndex=20&api_key=${RIOT_API}`
+      );
+      await delayAPI("소환사 매치정보 호출 완료");
+
+      matches.map((match, index) => {
+        newGameId[index] = match.gameId.toString();
+      });
+
+      for (let i = 0; i < newGameId.length; i++) {
+        if (preGameId.indexOf(newGameId[i]) === -1) {
+          addGameId = addGameId.concat(newGameId[i]);
+        }
+      }
+      console.log(addGameId);
+
+      for (let i = 0; i < preGameId.length; i++) {
+        if (newGameId.indexOf(preGameId[i]) === -1) {
+          delGameId = delGameId.concat(preGameId[i]);
+        }
+      }
+      console.log(delGameId);
+
+      addGameId.map(async add => {
+        await prisma.createDetail({
+          dGameId: add,
+          dSummoner: { connect: { sId } }
+        });
+      });
+
+      delGameId.map(async del => {
+        prisma.updateSummoner({
+          where: { sId },
+          data: { sDetail: { deleteMany: { dGameId: del } } }
+        });
+      });
+
+      for (let i = 0; i < addGameId.length; i++) {
+        const {
+          data: { participantIdentities, gameMode, gameType, participants }
+        } = await axios.get(
+          `https://kr.api.riotgames.com/lol/match/v4/matches/${addGameId[i]}?api_key=${RIOT_API}`
+        );
+        await delayAPI("소환사 디테일 " + (i + 1) + "번 호출 완료");
+        const arrayIndex = participantIdentities.findIndex(
+          x => x.player.accountId === sAccountId
+        );
+        await prisma.updateSummoner({
+          where: { sId },
+          data: {
+            sDetail: {
+              updateMany: {
+                where: { dGameId: addGameId[i] },
+                data: {
+                  dGameMode: gameMode,
+                  dGameType: gameType,
+                  dLane: participants[arrayIndex].timeline.lane,
+                  dRole: participants[arrayIndex].timeline.role,
+                  dKills: participants[arrayIndex].stats.kills,
+                  dDeaths: participants[arrayIndex].stats.deaths,
+                  dAssists: participants[arrayIndex].stats.assists,
+                  dWins: participants[arrayIndex].stats.win,
+                  dChampionId: participants[arrayIndex].championId
+                }
+              }
+            }
+          }
+        });
+        // await prisma.updateDetail({
+        //   where: {},
+        //   data: {
+        //     dGameMode: gameMode,
+        //     dGameType: gameType,
+        //     dLane: participants[arrayIndex].timeline.lane,
+        //     dRole: participants[arrayIndex].timeline.role,
+        //     dKills: participants[arrayIndex].stats.kills,
+        //     dDeaths: participants[arrayIndex].stats.deaths,
+        //     dAssists: participants[arrayIndex].stats.assists,
+        //     dWins: participants[arrayIndex].stats.win,
+        //     dChampionId: participants[arrayIndex].championId
+        //   }
+        // });
+      }
+      await delayAPI("호출 종료");
+
+      return true;
     }
   }
 };
