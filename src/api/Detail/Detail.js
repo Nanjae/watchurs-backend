1cccac8d6b4910fdc0781a6d79d3649649c5edef
Nanjae <33223057+Nanjae@users.noreply.github.com>
2020-05-25 18:42:58 +0900

    Clean up

--- src/api/Detail/Detail.js
+++
@@ -1,7 +0,0 @@
-import { prisma } from "../../../generated/prisma-client";
-
-export default {
-  Detail: {
-    dSummoner: ({ id }) => prisma.detail({ id }).dSummoner()
-  }
-};
--- src/server.js
+++ src/server.js
@@ -2,27 +2,15 @@ import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
-import { refreshState, serverRefresh } from "./serverRefresh";
-// import { athenticateJwt } from "./passport";
-// import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-  schema
+  schema,
   // context: ({ request }) => ({ request, isAuthenticated })
 });
 
-if (process.env.IS_DEV === "false") {
-  setInterval(() => {
-    if (!refreshState) {
-      serverRefresh();
-    }
-  }, 10000);
-}
-
 server.express.use(logger("dev"));
-// server.express.use(athenticateJwt);
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
